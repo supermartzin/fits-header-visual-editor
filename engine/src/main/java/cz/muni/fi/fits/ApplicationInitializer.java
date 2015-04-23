40308aeb65dfd75d7a274fc6ef6d12195d21db11
Martin Vrábel <supermartzin@gmail.com>
2015-04-23 18:49:11 +0200

    Added support for configuration through .properties file: added properties and configuration loaders and edited initialization phase

--- engine/src/main/java/cz/muni/fi/fits/ApplicationInitializer.java
+++ engine/src/main/java/cz/muni/fi/fits/ApplicationInitializer.java
@@ -2,23 +2,41 @@ package cz.muni.fi.fits;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import cz.muni.fi.fits.exceptions.ConfigurationException;
+import cz.muni.fi.fits.utils.loaders.ConfigurationLoader;
+import cz.muni.fi.fits.utils.loaders.PropertiesLoader;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * Main initializing class of FITS Header Editor Tool
  *
  * @author Martin Vrábel
- * @version 1.0
+ * @version 1.1
  */
 public class ApplicationInitializer {
 
     public static void main(String[] args) {
-        // inject all necessary dependencies
-        Injector injector = Guice.createInjector(new AppInjector(args));
+        try {
+            // load properties
+            //Properties properties = PropertiesLoader.loadProperties(ApplicationInitializer.class, "/fits.properties");     // for IDE
+            Properties properties = PropertiesLoader.loadProperties("./fits.properties");                                // for JAR
 
-        // get instance of executive class
-        FITSHeaderEditor editor = injector.getInstance(FITSHeaderEditor.class);
+            // load configuration
+            Map<String, String> config = ConfigurationLoader.loadConfiguration(properties);
 
-        // start FITS header edit operation
-        editor.start();
+            // inject all necessary dependencies
+            Injector injector = Guice.createInjector(new AppInjector(args, config));
+
+            // get instance of executive class
+            FITSHeaderEditor editor = injector.getInstance(FITSHeaderEditor.class);
+
+            // start FITS header edit operation
+            editor.start();
+        } catch (ConfigurationException | IOException ex) {
+            System.err.println(ex.getMessage());
+        }
     }
 }
