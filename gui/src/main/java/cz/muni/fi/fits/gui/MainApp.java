4140d7058d44422dd40643109ff30d3615bbf9e3
Martin Vrábel <supermartzin@gmail.com>
2015-04-24 23:42:17 +0200

    FITS Header Visual Editor Tool - Basic structure

---
+++ gui/src/main/java/cz/muni/fi/fits/gui/MainApp.java
@@ -0,0 +1,22 @@
+package cz.muni.fi.fits.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * TODO description
+ *
+ * @author Martin Vrábel
+ * @version 1.0
+ */
+public class MainApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ gui/src/main/java/cz/muni/fi/fits/gui/view/controllers/RootLayoutController.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.fits.gui.view.controllers;
+
+/**
+ * TODO description
+ */
+public class RootLayoutController {
+}
