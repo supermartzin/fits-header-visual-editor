b2acc49e011ca5c790ceb4d2e54b7fb8253912f6
Martin Vrábel <supermartzin@gmail.com>
2015-10-22 23:41:34 +0200

    Added user input validation and parsing into InputData object for first 6 operations + other changes

---
+++ gui/src/java/cz/muni/fi/fits/gui/models/inputdata/ChangeKeywordInputData.java
@@ -0,0 +1,10 @@
+package cz.muni.fi.fits.gui.models.inputdata;
+
+/**
+ * TODO insert description
+ *
+ * @author Martin Vrábel - © 2015 FITS-HeaderVisualEditor
+ * @version 1.0
+ */
+public class ChangeKeywordInputData {
+}
---
+++ gui/src/java/cz/muni/fi/fits/gui/models/inputdata/InputData.java
@@ -0,0 +1,22 @@
+package cz.muni.fi.fits.gui.models.inputdata;
+
+/**
+ * TODO insert description
+ *
+ * @author Martin Vrábel
+ * @version 1.0
+ */
+public interface InputData {
+
+    /**
+     *
+     * @return
+     */
+    String getInputDataString();
+
+    /**
+     *
+     * @param filePath
+     */
+    void setInputFilePath(String filePath);
+}
